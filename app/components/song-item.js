4a39482af499d34d03efc942c228eaff5849486f
Cesar Rodriguez <cesarrod@usc.edu>
2016-10-11 13:51:34 -0700

    Add styles, like button

--- app/components/song-item.js
+++ app/components/song-item.js
@@ -9,6 +9,23 @@ export default Ember.Component.extend({
 	isAffordable: Ember.computed('song.price', function(){
 		return parseFloat(this.get( 'song.price' )) < 1.00;
 		
-	})
+	}),
+	isLiked: false,
+	actions: {
+		like: function(e){
+			e.preventDefault();
+			//alert("Liked");
+			// console.log(  );
+			// if( $(this).hasClass( "liked" ) )
+				// $(this).removeClass( "liked" );
+			// else
+				// $(this).addClass( "liked" );
+			
+			if(this.get('isLiked'))
+				this.set('isLiked', false);
+			else
+				this.set('isLiked', true);
+		}
+	}
 	
 });
